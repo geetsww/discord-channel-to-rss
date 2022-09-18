58b704bf479b6dd29382165d38ba32e5da8e6977
Benjamin Geets <benjamingeets@tutamail.com>
2022-09-18 18:54:22 +0200

    fix: remove quote

--- index.js
+++ index.js
@@ -17,8 +17,8 @@ const getRSS = (item) => {
         if (current.embeds.length > 0) {
             content = content + `   <item>
         <title><![CDATA[${current.embeds[0].title ?? current.embeds[0].description} (${current.author.username})]]></title>
-        <link>${current.embeds[0].url}"</link>
-        <guid>${current.embeds[0].url}"</guid>
+        <link>${current.embeds[0].url}</link>
+        <guid>${current.embeds[0].url}</guid>
         <description><![CDATA[<p>${current.embeds[0].description}</p>]]></description>
         <pubDate>${current.timestamp}</pubDate>
      </item>`
